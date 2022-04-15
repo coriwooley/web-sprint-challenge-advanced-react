7d52ee5e6941161253d7c8d9242ce9d20cc702fb
Cori Wooley <wooleycori@gmail.com>
2022-04-15 02:06:07 -0800

    APP-FUNCTIONAL created state, created directions, mapped over and rendered grid from matrix

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,102 +1,101 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
-const URL = 'http://localhost:9000/api/result'
+const URL = "http://localhost:9000/api/result";
 
 export default class AppClass extends React.Component {
   state = {
     x: 2,
     y: 2,
     steps: 0,
-    email: '',
-    message: '',
+    email: "",
+    message: "",
     matrix: [
-      [0,0,0],
-      [0,1,0],
-      [0,0,0]
-    ]
-  }
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ],
+  };
 
   moveUp = () => {
     if (this.state.y === 1) {
       this.setState({
         ...this.state,
-        message: "You can't go up"
-      })
+        message: "You can't go up",
+      });
     } else {
-    const newMatrix = [...this.state.matrix]
-    newMatrix[this.state.y - 1][this.state.x - 1] = 0
-    newMatrix[this.state.y - 2][this.state.x - 1] = 1
-    this.setState({
-      ...this.state,
-      matrix: [...newMatrix],
-      y: this.state.y - 1,
-      steps: this.state.steps + 1,
-      message: ''
-    })
-  }
-  }
+      const newMatrix = [...this.state.matrix];
+      newMatrix[this.state.y - 1][this.state.x - 1] = 0;
+      newMatrix[this.state.y - 2][this.state.x - 1] = 1;
+      this.setState({
+        ...this.state,
+        matrix: [...newMatrix],
+        y: this.state.y - 1,
+        steps: this.state.steps + 1,
+        message: "",
+      });
+    }
+  };
 
   moveDown = () => {
     if (this.state.y === 3) {
       this.setState({
         ...this.state,
-        message: "You can't go down"
-      })
+        message: "You can't go down",
+      });
     } else {
-    const newMatrix = [...this.state.matrix]
-    newMatrix[this.state.y - 1][this.state.x - 1] = 0
-    newMatrix[this.state.y][this.state.x - 1] = 1
-    this.setState({
-      ...this.state,
-      matrix: [...newMatrix],
-      y: this.state.y + 1,
-      steps: this.state.steps + 1,
-      message: ''
-    })
-  }
-  }
+      const newMatrix = [...this.state.matrix];
+      newMatrix[this.state.y - 1][this.state.x - 1] = 0;
+      newMatrix[this.state.y][this.state.x - 1] = 1;
+      this.setState({
+        ...this.state,
+        matrix: [...newMatrix],
+        y: this.state.y + 1,
+        steps: this.state.steps + 1,
+        message: "",
+      });
+    }
+  };
 
   moveLeft = () => {
     if (this.state.x === 1) {
       this.setState({
         ...this.state,
-        message: "You can't go left"
-      })
+        message: "You can't go left",
+      });
     } else {
-    const newMatrix = [...this.state.matrix]
-    newMatrix[this.state.y - 1][this.state.x - 1] = 0
-    newMatrix[this.state.y - 1][this.state.x - 2] = 1
-    this.setState({
-      ...this.state,
-      matrix: [...newMatrix],
-      x: this.state.x - 1,
-      steps: this.state.steps + 1,
-      message: ''
-    })
-  }
-  }
-
+      const newMatrix = [...this.state.matrix];
+      newMatrix[this.state.y - 1][this.state.x - 1] = 0;
+      newMatrix[this.state.y - 1][this.state.x - 2] = 1;
+      this.setState({
+        ...this.state,
+        matrix: [...newMatrix],
+        x: this.state.x - 1,
+        steps: this.state.steps + 1,
+        message: "",
+      });
+    }
+  };
 
   moveRight = () => {
     if (this.state.x === 3) {
       this.setState({
         ...this.state,
-        message: "You can't go right"
-      })
+        message: "You can't go right",
+      });
     } else {
-    const newMatrix = [...this.state.matrix]
-    newMatrix[this.state.y - 1][this.state.x - 1] = 0
-    newMatrix[this.state.y - 1][this.state.x] = 1
-    this.setState({
-      ...this.state,
-      matrix: [...newMatrix],
-      x: this.state.x + 1,
-      steps: this.state.steps + 1,
-      message: ''
-    })
-  }
-  }
+      const newMatrix = [...this.state.matrix];
+      newMatrix[this.state.y - 1][this.state.x - 1] = 0;
+      newMatrix[this.state.y - 1][this.state.x] = 1;
+      this.setState({
+        ...this.state,
+        matrix: [...newMatrix],
+        x: this.state.x + 1,
+        steps: this.state.steps + 1,
+        message: "",
+      });
+    }
+  };
 
   resetGrid = () => {
     this.setState({
@@ -104,71 +103,94 @@ export default class AppClass extends React.Component {
       x: 2,
       y: 2,
       steps: 0,
-      email: '',
-      message: '',
+      email: "",
+      message: "",
       matrix: [
-        [0,0,0],
-        [0,1,0],
-        [0,0,0]
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
       ],
- 
-    })
-  }
+    });
+  };
 
   changeInput = (evt) => {
     this.setState({
       ...this.state,
-      email: evt.target.value
-    })
-  }
+      email: evt.target.value,
+    });
+  };
 
   onSubmit = (evt) => {
-    evt.preventDefault()
-    axios.post(URL, {...this.state, email: this.state.email})
-    .then((res) => {
-      this.setState({...this.state, message: res.data.message})
-    })
-    .catch((err) => {
-      this.setState({...this.state, message: err.response.data.message})
-    })
-    this.setState({...this.state, email:''})
-  }
-
-
+    evt.preventDefault();
+    axios
+      .post(URL, { ...this.state, email: this.state.email })
+      .then((res) => {
+        this.setState({ ...this.state, message: res.data.message });
+      })
+      .catch((err) => {
+        this.setState({ ...this.state, message: err.response.data.message });
+      });
+    this.setState({ ...this.state, email: "" });
+  };
 
   render() {
-    const {x, y, steps, message, matrix, email} = this.state
-    const { className } = this.props
+    const { x, y, steps, message, matrix, email } = this.state;
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates ({x},{y})</h3>
-          <h3 id="steps">You moved {steps} {steps === 1 ? 'time' : 'times'} </h3>
+          <h3 id="coordinates">
+            Coordinates ({x},{y})
+          </h3>
+          <h3 id="steps">
+            You moved {steps} {steps === 1 ? "time" : "times"}{" "}
+          </h3>
         </div>
         <div id="grid">
-          {
-            matrix.flatMap(square => square).map((squ, idx) => {
-              return <div key={idx} className={`square ${squ === 1 ? 'active' : ''} `}>
-                {squ === 1 ? 'B' : ''}
-              </div>
-            })
-          }
+          {matrix
+            .flatMap((square) => square)
+            .map((squ, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className={`square ${squ === 1 ? "active" : ""} `}
+                >
+                  {squ === 1 ? "B" : ""}
+                </div>
+              );
+            })}
         </div>
         <div className="info">
           <h3 id="message">{message}</h3>
         </div>
         <div id="keypad">
-          <button onClick={this.moveLeft} id="left">LEFT</button>
-          <button onClick={this.moveUp} id="up">UP</button>
-          <button onClick={this.moveRight} id="right">RIGHT</button>
-          <button onClick={this.moveDown} id="down">DOWN</button>
-          <button onClick={this.resetGrid} id="reset">reset</button>
+          <button onClick={this.moveLeft} id="left">
+            LEFT
+          </button>
+          <button onClick={this.moveUp} id="up">
+            UP
+          </button>
+          <button onClick={this.moveRight} id="right">
+            RIGHT
+          </button>
+          <button onClick={this.moveDown} id="down">
+            DOWN
+          </button>
+          <button onClick={this.resetGrid} id="reset">
+            reset
+          </button>
         </div>
         <form onSubmit={this.onSubmit}>
-          <input onChange={this.changeInput} value={email} id="email" type="email" placeholder="type email"></input>
+          <input
+            onChange={this.changeInput}
+            value={email}
+            id="email"
+            type="email"
+            placeholder="type email"
+          ></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
-    )
+    );
   }
 }
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,22 +1,132 @@
-import React from 'react'
+import React, {useState} from "react";
+import axios from "axios";
+
+const URL = "http://localhost:9000/api/result";
 
 export default function AppFunctional(props) {
+  const [state, setState] = useState({
+    message: "",
+    email: "",
+    steps: 0,
+    x: 2,
+    y: 2,
+    matrix: [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ],
+  });
+
+  const moveUp = () => {
+    if (state.y === 1) {
+      setState({
+        ...state,
+        message: "You can't go up",
+      });
+    } else {
+      const newMatrix = [...state.matrix];
+      newMatrix[state.y - 1][state.x - 1] = 0;
+      newMatrix[state.y - 2][state.x - 1] = 1;
+      setState({
+        ...state,
+        matrix: newMatrix,
+        y: state.y - 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    }
+  };
+
+  const moveDown = () => {
+    if (state.y === 3) {
+      setState({
+        ...state,
+        message: "You can't go down",
+      });
+    } else {
+      const newMatrix = [...state.matrix];
+      newMatrix[state.y - 1][state.x - 1] = 0;
+      newMatrix[state.y][state.x - 1] = 1;
+      setState({
+        ...state,
+        matrix: newMatrix,
+        y: state.y + 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    }
+  };
+
+  const moveLeft = () => {
+    if (state.x === 1) {
+      setState({
+        ...state,
+        message: "You can't go left",
+      });
+    } else {
+      const newMatrix = [...state.matrix];
+      newMatrix[state.y - 1][state.x - 1] = 0;
+      newMatrix[state.y - 1][state.x - 2] = 1;
+      setState({
+        ...state,
+        matrix: newMatrix,
+        x: state.x - 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    }
+  };
+
+  const moveRight = () => {
+    if (state.x === 3) {
+      setState({
+        ...state,
+        message: "You can't go right",
+      });
+    } else {
+      const newMatrix = [...state.matrix];
+      newMatrix[state.y - 1][state.x - 1] = 0;
+      newMatrix[state.y - 1][state.x] = 1;
+      setState({
+        ...state,
+        matrix: newMatrix,
+        x: state.x + 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    }
+  };
+
+  const resetGrid = () => {
+    setState({
+      ...state,
+      x: 2,
+      y: 2,
+      steps: 0,
+      email: "",
+      message: "",
+      matrix: [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+      ],
+    });
+  };
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({state.x}, {state.y})</h3>
+        <h3 id="steps">You moved {state.steps} {state.steps === 1 ? 'time' : 'times'}</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        {
+          state.matrix?.flatMap(square => square).map((squ, idx) => {
+            return <div key={idx} className={`square ${squ === 1 ? "active" : ""}`}>
+              {squ === 1 ? "B" : ""}
+            </div>
+          })
+        }
       </div>
       <div className="info">
         <h3 id="message"></h3>
@@ -33,5 +143,5 @@ export default function AppFunctional(props) {
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
